7f263c33a9bedf13f330fe9c6cc690a4b060d46a
Ntobeko <sitholent@gmail.com>
2022-07-12 23:21:27 +0200

    Fix Betty style error

--- sub__printfF.c
+++ sub__printfF.c
@@ -4,23 +4,22 @@
  * select_formatter - selects the format specifier.
  * @s: pointer to the string of the format.
  *
- * Return: number of bytes printed
+ * Return: number of bytes printed.
  */
 
 int (*select_formatter(char *s))(va_list)
 {
-    formatter_t cp[] = {
-        {"c", print_c},
-        {"s", print_s},
-        {"%", print_percent},
-    };
-    int i;
+	formatter_t cp[] = {
+		{"c", print_c},
+		{"s", print_s},
+		{"%", print_percent},
+	};
+	int i;
 
-    for (i = 0; cp[i].s != NULL; i++)
-    {
-        if (*(cp[i].s) == *s)
-            break;
-    }
-
-    return (cp[i].f);
+	for (i = 0; cp[i].s != NULL; i++)
+	{
+		if (*(cp[i].s) == *s)
+			break;
+	}
+	return (cp[i].f);
 }
